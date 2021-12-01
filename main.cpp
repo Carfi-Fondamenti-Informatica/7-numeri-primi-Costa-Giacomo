dd7c76d532c13ae844fa0c5ea1c48c611d3ac510
Costa-Giacomo <92380404+Costa-Giacomo@users.noreply.github.com>
2021-12-01 11:55:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-
 int main(){
     int a=0, b=2;
     cin >> a;
-
     if ( numero_primo (a, b) ){
         cout << "numero primo";
     }else{
