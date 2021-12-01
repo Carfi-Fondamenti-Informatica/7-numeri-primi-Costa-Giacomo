a674913386ce8ba147adc1e7d30a8dbcb6c1f42c
Costa-Giacomo <92380404+Costa-Giacomo@users.noreply.github.com>
2021-12-01 11:51:43 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include "lib.h"
 
+int numero_primo (int a, int b){
+
+    if( a==1 ){
+        return 0;
+    }
+
+    if( a%b == 0 && a==b){
+        return 1;
+    }else if ( ( a%b==0 ) && ( a!=b ) ){
+        return 0;
+    }else{
+        return numero_primo (a, (b+1));
+    }
+}
